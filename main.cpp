dacfb0b18d9bbb9f8fff71512b8f41cbb4044a69
Bothij <bothij@gmail.com>
2020-12-03 19:57:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//Write your function declaration here.
+int CalculateRectangleArea(int width, int height);
+
+int main() {
+CalculateRectangleArea(2, 4); {
+  std::cout << "A négyszög területe " << CalculateRectangleArea(5, 7) << " négyzetméter." << std::endl;
+  std::string PrintPoem(); {
+  std::cout << "Egy – megérett a meggy," << std::endl;
+  return "stop!";
+  std::cout << "Kettõ – csipkebokorvesszõ," << std::endl;
+  std::cout << "Három – te vagy az én párom," << std::endl;
+}
+}
+
+//Write your function definition here.
+int CalculateRectangleArea(int width, int height) {
+  return width * height * height;
+  
+}
+ 
+
